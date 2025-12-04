2b83b699a5f306a51498ca289427a35ac5d743a1
SebaAlshedokhi <sebashedokhi@gmail.com>
2025-12-05 01:35:27 +0300

    Create 12-roman_to_int.py

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    int_sum = 0
+    if not isinstance(roman_string, str):
+        return 0
+    roman_dict = {"I" : 1, "V" : 5, "X" : 10, "L" : 50, "C" : 100, "D" : 500, "M" : 1000}
+    for i in range(len(roman_string) - 1):
+        if roman_dict.get(roman_string[i], 0) == 0:
+            return 0
+        if roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]:
+            int_sum -= roman_dict[roman_string[i]]
+        else:
+            int_sum += roman_dict[roman_string[i]]
+    int_sum += roman_dict[roman_string[i]]
+    return int_sum
