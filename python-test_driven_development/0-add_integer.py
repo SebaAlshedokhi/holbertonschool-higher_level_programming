6430d07c6d9c97a094c2ab69a50bafe0d8a8dd29
SebaAlshedokhi <sebashedokhi@gmail.com>
2025-12-06 03:59:36 +0300

    Update 0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,29 +1,18 @@
 #!/usr/bin/python3
-"""
-
-Add Integer or Float Module
-
-"""
-
-
+"""Defines an integer addition function"""
 def add_integer(a, b=98):
-    """
-    ADD Two integer a and b
-
-    Args:
-        a (int/float): first int
-        b (int/float): Second int
-
+    """Return the integer addition of a and b
+    Float arguments are typecasted to ints before addition is performed.
     Raises:
-        TypeError: in case the arguments are not int or float
-
-    Return:
-        (int) : Sum of the int a and b
+        TypeError: If either of a or b is a non-integer and non-float.
+        OverflowError: If float infinity or NaN is passed.
     """
-    if type(a) not in [int, float]:
-        raise TypeError('a must be an integer')
-    if type(b) not in [int, float]:
-        raise TypeError('b must be an integer')
-    if (a + b) == float('inf') or (a + b) == -float('inf'):
-        return b
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    if isinstance(a, float) and (a == float('inf') or a == float('-inf') or a != a):
+        raise OverflowError("cannot convert float infinity to integer")
+    if isinstance(b, float) and (b == float('inf') or b == float('-inf') or b != b):
+        raise OverflowError("cannot convert float infinity to integer")
     return int(a) + int(b)
