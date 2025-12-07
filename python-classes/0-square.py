d57e668452bd9a6814984a4a8dfaf6d499a92808
SebaAlshedokhi <sebashedokhi@gmail.com>
2025-12-07 18:41:44 +0300

    Update 0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-"""an empty class Square that defines a square"""
-""" first task just to make a class to define a square """
+"""
+an empty class Square that defines a square
+first task just to make a class to define a square """
 
 class Square:
-    """an empty class"""
+    """""""""""""""
+    an empty class
+    """""""""""""""
+    
     pass
