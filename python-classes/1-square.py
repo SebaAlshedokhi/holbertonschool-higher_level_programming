78b104760f2ee91b38ffa623cf9615b3b95d37db
SebaAlshedokhi <sebashedokhi@gmail.com>
2025-12-07 19:29:20 +0300

    Create 1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""""""""""""""""""""""""""
+a class Square that defines a square by: (based on 0-square.py)
+
+"""""""""""""""""""""""""""
+
+class Square (Square):
+"""""""""""""""""""""""""""
+for a private attribute size
+"""""""""""""""""""""""""""
+    __size
