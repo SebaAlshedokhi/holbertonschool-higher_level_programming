f67291ab757efbc280d96a9a8dc715ed2ee4cfa0
SebaAlshedokhi <sebashedokhi@gmail.com>
2025-12-07 18:52:31 +0300

    Update 0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """
+Module 0-square
 an empty class Square that defines a square
-first task just to make a class to define a square """
+first task just to make a class to define a square
+"""
 
 class Square:
     """""""""""""""
