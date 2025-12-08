9b55ea98c8b5adf0389d73463f2582009fcc7eba
SebaAlshedokhi <sebashedokhi@gmail.com>
2025-12-08 19:34:08 +0300

    Update 5-square.py

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -46,3 +46,5 @@ class Square:
             for i in range(self.__size):
                 for j in range(self.__size):
                     print("#", end="")
+                if i != self.__size - 1:
+                    print()
