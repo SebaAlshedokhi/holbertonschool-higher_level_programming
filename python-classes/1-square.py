7a605d3d242b98a197364c17ff519cf59a1e8e9e
SebaAlshedokhi <sebashedokhi@gmail.com>
2025-12-08 15:50:20 +0300

    Update 1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -2,9 +2,11 @@
 """
 a class Square that defines a square by: (based on 0-square.py)
 """
+
 class Square:
 """
 initilize a private attribute size
 """
+
       def __init__(self, size):
           self.__size = size
