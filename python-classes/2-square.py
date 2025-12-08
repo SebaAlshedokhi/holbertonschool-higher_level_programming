01ad98289af591f5afb491113d495c779aa4ede5
SebaAlshedokhi <sebashedokhi@gmail.com>
2025-12-08 18:26:12 +0300

    Update 2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -6,17 +6,13 @@ class Square:
     Instantiation with optional size
     Private instance attribute: size
     """
-
     def __init__(self, size=0):
         """
         check size if integer or not
         then check size if less than zero
-        then assign size
         """
-        if not isinstance(size, int)
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-            break
         if size < 0:
             raise ValueError("size must be >= 0")
-            break
         self.__size = size
