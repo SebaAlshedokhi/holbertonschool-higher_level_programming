a9de2a34b16d73034a7c1fcf1d6a7225e4d1e646
SebaAlshedokhi <sebashedokhi@gmail.com>
2025-12-11 17:40:39 +0300

    Implement Rectangle class with area and perimeter methods

---
+++ python-more_classes/3-rectangle.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python3
+"""define a rectangle"""
+
+
+class Rectangle:
+    """
+    defines a rectangle by two private instance attributes
+    width and height
+    """
+
+    def __init__(self, width=0, height=0):
+        """
+        Initializes a new Square instance.
+        arguments are width and height
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """
+        to retrieve the width
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        check it integer and greater or equal zero
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+        to retrieve the height
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        check it integer and greater or equal zero
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """
+        returns the current rectangle area
+        """
+        return self.width * self.height
+
+    def perimeter(self):
+        """
+        returns the rectangle perimeter
+        """
+        if self.width == 0 or self.height == 0:
+            return 0
+        return 2 * (self.width + self.height)
+        
+    def __print__(self):
+        """
+        Print the rectangle using the '#' character.
+        if width or height is equal to 0, return an empty string
+        """
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
+        """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+    def __str__(self):
+        """
+        str constructor
+        """
+        return f"Name: {self.name}, Age: {self.age}"
+        
+        if self.width == 0 or self.height == 0:
+            return 
