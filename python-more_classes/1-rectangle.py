260a8d7d2ab8322516120d3416a4d471e4bdbfec
SebaAlshedokhi <sebashedokhi@gmail.com>
2025-12-11 15:32:24 +0300

    Clean up blank lines in rectangle class
    Removed unnecessary blank lines in the rectangle class.

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -16,14 +16,12 @@ class Rectangle:
         self.width = width
         self.height = height
 
-
     @property
     def width(self):
         """
         to retrieve the width
         """
         return self.__width
-
 
     @width.setter
     def width(self, value):
@@ -36,14 +34,12 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         self.__width = value
 
-
     @property
     def height(self):
         """
         to retrieve the height
         """
         return self.__height
-
 
     @height.setter
     def height(self, value):
