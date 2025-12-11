106ba77249ba95a315b818be73ba4c05ee825cb1
SebaAlshedokhi <sebashedokhi@gmail.com>
2025-12-11 17:05:16 +0300

    Fix syntax error in perimeter method

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -62,7 +62,7 @@ class Rectangle:
         """
         returns the rectangle perimeter
         """
-        if self.width == 0 or self.height == 0
+        if self.width == 0 or self.height == 0:
             return 0
         return 2 * (self.width + self.height)
 
