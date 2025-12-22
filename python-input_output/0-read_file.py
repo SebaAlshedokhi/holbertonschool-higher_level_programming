43fee016b0a1f1e11239c65d8cbcb1825904c681
SebaAlshedokhi <sebashedokhi@gmail.com>
2025-12-22 16:06:02 +0300

    Create 0-read_file.py

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Read file"""
+
+
+def read_file(filename=""):
+    """
+    a function that reads a text file and prints it
+    """
+    with open(filename, encoding="utf-8") as f:
+        read_data = f.read()
