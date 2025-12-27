b0e7454ca1326143396a84d0314ec0fe9f513c76
SebaAlshedokhi <sebashedokhi@gmail.com>
2025-12-27 16:56:55 +0300

    Create 9-student.py

---
+++ python-input_output/9-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""defining Student class"""
+
+
+class Student:
+    """
+    represent a student with personal information.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialize a new Student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        return dictionary representation of Student.
+        """
+        return self.__dict__
