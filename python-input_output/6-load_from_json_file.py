f7e3f2bac0fb9b62eb000523d6236a78d1f52cfa
SebaAlshedokhi <sebashedokhi@gmail.com>
2025-12-27 16:49:43 +0300

    Update 6-load_from_json_file.py

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-"""creates an Object"""
-import json
+"""Script to add command line arguments to a list in JSON file"""
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+filename = "add_item.json"
 
-def load_from_json_file(filename):
-    """
-    function that creates an Object from a “JSON file”
-    """
-    with open(filename, 'r') as f:
-        return json.load(f)
+try:
+    items = load_from_json_file(filename)
+except FileNotFoundError:
+    items = []
+
+items.extend(sys.argv[1:])
+save_to_json_file(items, filename)
