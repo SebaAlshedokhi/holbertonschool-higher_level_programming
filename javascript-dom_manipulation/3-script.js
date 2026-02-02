057f78a0f36b58ac635294e50524e17b0cc2b9b3
SebaAlshedokhi <sebashedokhi@gmail.com>
2026-02-02 18:57:33 +0300

    Update 3-script.js

--- javascript-dom_manipulation/3-script.js
+++ javascript-dom_manipulation/3-script.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+
+const head = document.querySelector('header')
+const togg = document.querySelector('#toggle_header')
+
+tog.addEventListener('click', () => {
+  if(head.classList.contains('red'))
+  {
+    head.classList.remove('red');
+    head.classList.add('green');
+  }
+  else
+  {
+    head.classList.remove('green');
+    head.classList.add('red');
+  };
+});
