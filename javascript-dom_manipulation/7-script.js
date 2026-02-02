4c514b3405fe08c8840428e8972f0c3174eedfe8
SebaAlshedokhi <sebashedokhi@gmail.com>
2026-02-02 19:54:56 +0300

    Update 7-script.js

--- javascript-dom_manipulation/7-script.js
+++ javascript-dom_manipulation/7-script.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+
+const list = document.querySelector('#list_movies');
+
+fetch('https://swapi-api.hbtn.io/api/films/?format=json')
+  .then(response => response.json())
+  .then(data => {
+    data.results.forEach(movie => {
+      const li = document.createElement('li');
+      li.textContent = movie.title;
+      list.appendChild(li);
+    });
+  });
