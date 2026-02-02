660fdc07c3e0285c31b16a898143377c65013e81
SebaAlshedokhi <sebashedokhi@gmail.com>
2026-02-02 19:23:09 +0300

    Update 4-script.js

--- javascript-dom_manipulation/4-script.js
+++ javascript-dom_manipulation/4-script.js
@@ -1,2 +1,10 @@
 #!/usr/bin/node
 
+const item = document.querySelector('#add_item');
+const list = document.querySelector('.my_list');
+
+item.addEventListener('click', () => {
+  const newElement = document.createElement('li');  //createElement just with doc not element
+  newElement.textContent = 'Item';
+  list.appendChild(newElement);
+});
