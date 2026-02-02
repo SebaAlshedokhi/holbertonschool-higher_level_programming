340435eed4dd18dad61bb95dd0789f379a1385d1
SebaAlshedokhi <sebashedokhi@gmail.com>
2026-02-02 19:34:55 +0300

    Update 5-script.js

--- javascript-dom_manipulation/5-script.js
+++ javascript-dom_manipulation/5-script.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+
+const head = document.querySelector('header');
+const UpdateHeader = document.querySelector('#update_header');
+
+UpdateHeader.addEventListener('click', () => {
+  head.textContent = 'New Header!!!';
+});
