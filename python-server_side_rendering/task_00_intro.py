8866467d197ce372f6a71dd344214cd223dabba7
SebaAlshedokhi <sebashedokhi@gmail.com>
2026-02-02 21:03:39 +0300

    Update task_00_intro.py

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -1,1 +1,7 @@
-
+def generate_invitations(template, attendees):
+    try:         #تتأكد من نوع البيانات المدخلة
+        isinstance(template, 'string')
+        isinstance(attendees, 'dict')
+    except ValueError:        #إذا غير كذا → اطبع رسالة خطأ وأوقف التنفيذ.
+        exit
+    
